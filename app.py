a8f6db4341b0e4791e7bece8dc428998c4652a32
叶之 <yuankai.myk@alibaba-inc.com>
2021-03-13 13:40:59 +0800

    before add socket

--- app.py
+++ app.py
@@ -40,6 +40,46 @@ def home_control():
 
 @app.route('/car_view/', methods=['GET', 'POST'])
 def car_view():
+    # 给前端页面展示的数据只能传到html文件中，而不能直接传到script中
+    return render_template("car_view.html", message_to_show="欢迎使用车辆监控平台")
+
+
+@app.route('/car_view/start_server')
+def start_server():
+    # encoding: utf-8
+    # import socket
+    # import time
+    # print("服务端开启")
+    # # 套接字接口
+    # mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # # 设置IP和端口
+    # host = '192.168.124.3'
+    # port = 2222
+    # # bind绑定该端口
+    # mySocket.bind((host, port))
+    # mySocket.listen(10)
+    # print("Before return")
+    # while True:
+    #     # 接收客户端连接
+    #     print("等待连接....")
+    #     client, address = mySocket.accept()
+    #     print("新连接")
+    #     print("IP is %s" % address[0])
+    #     print("port is %d\n" % address[1])
+    #     while True:
+    #         # 读取消息
+    #         msg = client.recv(1024)
+    #         # 把接收到的数据进行解码
+    #         print(msg.decode("utf-8"))
+    #         print("读取完成")
+    #         if msg == b"hello":
+    #             print("你好，老弟")
+    #         if msg == b"qq":
+    #             client.close()
+    #             mySocket.close()
+    #             print("程序结束\n")
+    #             exit()
+
     return render_template("car_view.html")
 
 
